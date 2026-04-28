529098dc0f5c98b5cc67f417150a27eaf328d138
Николай <156302814+kolyasochnick@users.noreply.github.com>
2026-04-28 20:58:38 +0300

    Time: 581 ms (8.98%), Space: 100.9 MB (31.37%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -0,0 +1,27 @@
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        q = deque([(0, 0, 0, 0)])
+        moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        visited = set()
+        n = len(grid)
+        m = len(grid[0])
+
+        while q:
+            x, y, px, py = q.popleft()
+            value = grid[y][x]
+            if (x, y) not in visited:
+                visited.add((x, y))
+                for dx, dy in moves:
+                    nx = x + dx
+                    ny = y + dy
+                    if 0 <= nx < m and 0 <= ny < n:
+                        if grid[ny][nx] == value:
+                            if (nx, ny) in visited and (nx, ny) != (px, py):
+                                return True
+                            if (nx, ny) not in visited:
+                                q.appendleft((nx, ny, x, y))
+                        else:
+                            if (nx, ny) not in visited:
+                                q.append((nx, ny, x, y))
+
+        return False
