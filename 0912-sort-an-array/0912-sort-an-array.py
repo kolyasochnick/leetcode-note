a74db30e43220fd50f79d5905f44ece2431856f6
Николай <156302814+kolyasochnick@users.noreply.github.com>
2026-05-08 13:17:29 +0300

    Time: 63 ms (86.94%), Space: 28.8 MB (23.72%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,22 +1,17 @@
 class Solution:
-    def _radixSort(self, nums):
-        if not nums:
-            return []
-        dec = 1
-        maxEl = max(nums)
-        while maxEl // dec > 0:
-            buckets = [[] for _ in range(10)]
-            for num in nums:
-                index = (num // dec) % 10
-                buckets[index].append(num)
-            nums = [num for bucket in buckets for num in bucket]
-            dec *= 10
-        return nums
-
     def sortArray(self, nums: List[int]) -> List[int]:
-
-        positive = self._radixSort(list(filter(lambda x: x >= 0, nums)))
-        negative = [-x for x in self._radixSort([-x for x in nums if x < 0])][::-1]
-
-        return negative + positive
+        mn = min(nums)
+        dif = max(nums) - min(nums) + 1
+        count = [0] * dif
+        for num in nums:
+            count[num - mn] += 1
         
+        res = []
+        for i in range(dif):
+            value = i + mn
+
+            for _ in range(count[i]):
+                res.append(value)
+                
+        return res
+
