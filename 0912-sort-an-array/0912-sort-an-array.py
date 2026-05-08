ab882b4fcd1c26f377acc845e1249e745e84155c
Николай <156302814+kolyasochnick@users.noreply.github.com>
2026-05-08 12:31:58 +0300

    Time: 667 ms (39.31%), Space: 31.6 MB (15.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,28 +1,33 @@
 class Solution:
+
+    def _merge(self, nums1, nums2):
+        i = 0
+        j = 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                res.append(nums2[j])
+                j += 1
+            elif nums1[i] < nums2[j]:
+                res.append(nums1[i])
+                i += 1
+            else:
+                res.append(nums2[j])
+                res.append(nums1[i])
+                i += 1
+                j += 1
+        res.extend(nums1[i:])
+        res.extend(nums2[j:])
+        return res
+
     def sortArray(self, nums: List[int]) -> List[int]:
+        q = deque([[x] for x in nums])
+
+        while len(q) > 1:
+            left = q.popleft()
+            right = q.popleft() if q else []
+
+            q.append(self._merge(left, right))
         
-        def radixSort(nums, negative=False):
-            if not nums:
-                return nums
-            if negative:
-                nums = [-num for num in nums]
-            place = 1
-            maxElem = max(nums)
-
-            while maxElem // place > 0:
-                buckets = [[] for _ in range(10)]
-
-                for num in nums:
-                    index = (num // place) % 10
-                    buckets[index].append(num)
-
-                nums = [num for bucket in buckets for num in bucket]
-                place *= 10
-            if negative:
-                return [-num for num in nums][::-1]
-            return nums
-        positive = radixSort(list(filter(lambda x: x>=0, nums)))
-        negative = radixSort(list(filter(lambda x: x<0, nums)), True)
-
-        return negative + positive
-
+        return q[0] if q else []
+        
